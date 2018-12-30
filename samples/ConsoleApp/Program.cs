91d4d5ade56953e0dc155426bb98f702670d8c71
Todd Skelton <todd.skelton@outlook.com>
2018-12-29 23:36:38 -0600

    Clean up sample

--- samples/ConsoleApp/Program.cs
+++ samples/ConsoleApp/Program.cs
@@ -1,9 +1,6 @@
 ﻿using ConsoleBuildR;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
 using System;
 using System.Threading;
-using System.Threading.Tasks;
 
 namespace ConsoleApp
 {
@@ -29,47 +26,5 @@ namespace ConsoleApp
             .Execute<Executable1>()
             .Execute<Executable2>()
             .Build();
-    }
-
-    public class Executable1 : IExecutable
-    {
-        private readonly ILogger _logger;
-        private readonly IConfiguration _configuration;
-
-        public Executable1(ILogger<Executable1> logger, IConfiguration configuration)
-        {
-            _logger = logger;
-            _configuration = configuration;
-        }
-
-        public Task Execute(string[] args)
-        {
-            Thread.Sleep(100);
-
-            _logger.LogInformation(_configuration.GetValue<string>("Message"));
-
-            return Task.CompletedTask;
-        }
-    }
-
-    public class Executable2 : IExecutable
-    {
-        private readonly ILogger _logger;
-        private readonly IConfiguration _configuration;
-
-        public Executable2(ILogger<Executable2> logger, IConfiguration configuration)
-        {
-            _logger = logger;
-            _configuration = configuration;
-        }
-
-        public Task Execute(string[] args)
-        {
-            Thread.Sleep(50);
-
-            _logger.LogInformation(_configuration.GetValue<string>("Message"));
-
-            return Task.CompletedTask;
-        }
     }
 }
